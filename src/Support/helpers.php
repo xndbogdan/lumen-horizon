173ae8c1bc94f4a5dd20ce2f652f1a8e920bfcc2
Charbel <charbel.el-jalkh@carsguide.com.au>
2020-04-17 16:56:53 +1000

    Add helper methods

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -7,10 +7,38 @@ if (! function_exists('resolve')) {
      * Resolve a service from the container.
      *
      * @param  string  $name
+     * @param  array  $parameters
      * @return mixed
      */
-    function resolve($name)
+    function resolve($name, array $parameters = [])
     {
-        return app($name);
+        return app($name, $parameters);
+    }
+}
+
+if (! function_exists('app_path')) {
+    /**
+     * Get the path to the application folder.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function app_path($path = '')
+    {
+        return app()->basePath() . '/app' . ($path ? '/' . $path : $path);
+    }
+}
+
+if ( ! function_exists('config_path'))
+{
+    /**
+     * Get the configuration path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
     }
 }
