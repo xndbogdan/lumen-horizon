9a92431d28e62d481f048fbc0efdaf51225665e2
Charbel <charbel.el-jalkh@carsguide.com.au>
2020-04-17 19:37:03 +1000

    Lumen Horizon 4

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -16,34 +16,6 @@ if (! function_exists('resolve')) {
     }
 }
 
-//if (! function_exists('app_path')) {
-//    /**
-//     * Get the path to the application folder.
-//     *
-//     * @param  string  $path
-//     * @return string
-//     */
-//    function app_path($path = '')
-//    {
-//        return app()->basePath() . '/app' . ($path ? '/' . $path : $path);
-//    }
-//}
-//
-//if ( ! function_exists('config_path'))
-//{
-//    /**
-//     * Get the configuration path.
-//     *
-//     * @param  string $path
-//     * @return string
-//     */
-//    function config_path($path = '')
-//    {
-//        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
-//    }
-//}
-
-
 if (! function_exists('public_path')) {
     /**
      * Get the path to the public folder.
