620a3ac0fdbabbc862bb2721cbe4ba8aa8d115ed
Charbel <charbel.el-jalkh@carsguide.com.au>
2020-04-22 19:38:17 +1000

    Remove unnecessary space

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -2,12 +2,12 @@
 
 /** Helper functions to provide better Lumen-Laravel compatibility for the package */
 
-if (! function_exists('resolve')) {
+if (!function_exists('resolve')) {
     /**
      * Resolve a service from the container.
      *
-     * @param  string  $name
-     * @param  array  $parameters
+     * @param string $name
+     * @param array $parameters
      * @return mixed
      */
     function resolve($name, array $parameters = [])
